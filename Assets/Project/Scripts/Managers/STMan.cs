cbf15a8fe85446aea57c9b2c5f4d5f6089667b3a
Jean-LoupMARTIN <jean_loup_martin@hotmail.com>
2021-01-07 05:47:51 +0100

    import

---
+++ Assets/Project/Scripts/Managers/STMan.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class STMan : MonoBehaviour
+{
+    public static STMan inst;
+
+    public float smoothTranslate_t;
+    public AnimationCurve smoothTranslate_curve;
+
+    public ARRaycastManager arRaycastManager;
+
+    public SmoothTranslate target;
+
+
+    void Awake() { inst = this; }
+
+
+    void Update()
+    {
+        if (target)
+        {
+            bool test = ProjectMan.test;
+            Camera cam = ProjectMan.inst.cam;
+            Vector3 pos;
+
+            if ((test && Tool.MouseHit(cam, out pos, ProjectMan.LayerMask_NAR_Ground)) ||
+                (!test && Tool.ScreenCenterHitAR(ProjectMan.inst.cam, arRaycastManager, out pos)))
+            {
+                // rotation
+                Vector3 camProj = cam.transform.position;
+                camProj.y = pos.y;
+                Tool.LookDir(target, Tool.Dir(camProj, pos, false));
+
+                // position
+                target.SetTarget(pos);
+
+                // active
+                if (!target.gameObject.active)
+                    target.gameObject.SetActive(true);
+            }
+
+
+            if (target.gameObject.active && Tool.Click())
+                target = null;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/SmoothTranslate.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+
+
+public class SmoothTranslate : MonoBehaviour
+{
+    float t;
+
+
+    Vector3 target, lastPos;
+
+    void Start() { t = STMan.inst.smoothTranslate_t; }
+
+    void Update()
+    {
+        if (t < STMan.inst.smoothTranslate_t)
+        {
+            t += Time.deltaTime;
+            float progress = Tool.Progress(t, STMan.inst.smoothTranslate_t);
+            if (progress >= 1) transform.position = target;
+            else transform.position = lastPos + (target - lastPos) * STMan.inst.smoothTranslate_curve.Evaluate(progress);
+        }
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        if (gameObject.active)
+        {
+            this.target = target;
+            lastPos = transform.position;
+            t = 0;
+        }
+        else transform.position = target;
+    }
+}
